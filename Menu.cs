3af8c544b8b8ab976bad051173587cc69ecff71e
fatha124 <niklas.lund@hotmail.se>
2019-10-19 00:33:04 +0100

    Add files via upload

--- Menu.cs
+++ Menu.cs
@@ -30,7 +30,7 @@ namespace Pragueparking2._01
             {
                 case "1":
                     Console.Clear();
-                    registry.Add();
+                    AddVehicle() ;
                     break;
                 case "2":
                     //SearchVehicle();
@@ -59,7 +59,77 @@ namespace Pragueparking2._01
                     MainMenu();
                     break;
             }
+      
+        
+            
         }
+
+        public  void AddVehicle()
+        {
+            DateTime TimeWhenParked = DateTime.Now;
+            Console.Clear();
+            Console.WriteLine("*******************************************\n" +
+                "Please enter the type of vehicle\n" + "{Car}\n" + "or\n" + "{MC}\n" + "*******************************************");
+            string type = Console.ReadLine();
+            type.ToLower();
+            if (type == "car" || type == "mc" || type == "m" || type == "c")
+            {
+                Console.WriteLine("Enter registration number: ");
+                string regnumb = Console.ReadLine();
+                regnumb.ToLower();
+                while (regnumb.Length != 6)
+                {
+                    Console.WriteLine("Invalid registration number.");
+                    regnumb = Console.ReadLine();
+                    regnumb.ToLower();
+                    Console.Clear();
+
+                }
+                if (registry.CheckForDup(regnumb) == false)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Choose parkingspot.");
+                    int parkspot = 0;
+                    string chosenspot = Console.ReadLine();
+                    int.TryParse(chosenspot, out parkspot);
+                    while (parkspot == 0 || parkspot > 100 || parkspot < 0)
+                    {
+                        Console.WriteLine("Invalid spot, choose a parkingspot between spot 1 - 100 ");
+                        int.TryParse(Console.ReadLine(), out parkspot);
+                        Console.Clear();
+                    }
+                    while (CheckIfSpotIsTaken(parkspot, type))
+                    {
+                        Console.WriteLine("The chosen spot {0} is  already taken\n" + "Please try again... ", parkspot);
+                        int.TryParse(Console.ReadLine(), out parkspot);
+                        while (parkspot == 0 || parkspot > 100 || parkspot < 0)
+                        {
+                            Console.WriteLine("Invalid spot, choose a parkingspot between spot 1 - 100 ");
+                            int.TryParse(Console.ReadLine(), out parkspot);
+                            Console.Clear();
+                        }
+                    }
+                    Console.Clear();
+                    InsertVehicle(type, regnumb, parkspot, TimeWhenParked);
+                    Console.WriteLine("Your {0} with regnumber {1} has been parked at spot {2}.\n" + " At the current time {3}\n" + "Press enter to continue...", type, regnumb, parkspot, TimeWhenParked);
+                    Console.ReadKey();
+                    MainMenu();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("{0} already exist in the system.\n" + "Press any key to continue", regnumb);
+                    Console.ReadKey();
+                    AddVehicle();
+                }
+            }
+            else
+            {
+                AddVehicle();
+            }
+            //Put code
+        }
+
     }
 }
 
