a9e1f2cf7212612a7183513438249b0ab613d6d1
fatha124 <niklas.lund@hotmail.se>
2019-10-18 23:59:19 +0100

    Add files via upload

--- Registry.cs
+++ Registry.cs
@@ -31,7 +31,52 @@ namespace Pragueparking2._01
         }
         public void Add()
         {
-            
+            DateTime TimeWhenParked = DateTime.Now;
+            Console.Clear();
+            Console.WriteLine("*******************************************\n" +
+                "Please enter the type of vehicle\n" + "{Car}\n" + "or\n" + "{MC}\n" + "*******************************************");
+            string type = Console.ReadLine();
+            type.ToLower();
+            if (type == "car" || type == "mc" || type == "m" || type == "c") 
+            {
+                Console.WriteLine("Enter registration number: ");
+                string regnumb = Console.ReadLine();
+                regnumb.ToLower();
+                while (regnumb.Length != 6) 
+                {
+                    Console.WriteLine("Invalid registration number.");
+                    regnumb = Console.ReadLine();
+                    regnumb.ToLower();
+                    Console.Clear();
+                }
+                if (CheckForDup(regnumb) == false) 
+                {
+                    Console.Clear();
+                    Console.WriteLine("Choose parkingspot.");
+                    int parkspot = 0;
+                    string chosenspot = Console.ReadLine();
+                    int.TryParse(chosenspot, out parkspot);
+                    while (parkspot == 0 || parkspot > 100 || parkspot < 0) 
+                    {
+                        Console.WriteLine("Invalid spot, choose a parkingspot between spot 1 - 100 ");
+                        int.TryParse(Console.ReadLine(), out parkspot);
+                        Console.Clear();
+                    }
+                    while (CheckIfSpotIsTaken(parkspot, type)) 
+                    {
+                        Console.WriteLine("The chosen spot {0} is  already taken\n" + "Please try again... ", parkspot);
+                        int.TryParse(Console.ReadLine(), out parkspot);
+                    }
+                    while (parkspot == 0 || parkspot > 100 || parkspot < 0) 
+                    {
+                        Console.WriteLine("Invalid spot, choose a parkingspot between spot 1 - 100 ");
+                        int.TryParse(Console.ReadLine(), out parkspot);
+                        Console.Clear();
+                    }
+                 }
+                Console.Clear();
+                RegisterVehicle(type,regnumb,parkspot);
+            }
         }
 
         public bool Search() 
