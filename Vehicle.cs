54d03ca1ed11523ae9ee9ed62e588a4ae9453f0b
fatha124 <niklas.lund@hotmail.se>
2019-10-18 23:02:09 +0100

    Add files via upload

--- Vehicle.cs
+++ Vehicle.cs
@@ -4,13 +4,15 @@ namespace Pragueparking2._01
 {
     public class Vehicle
     {
+        internal static int parkspot;
+
         public string TypeOfVehicle { get; set; }
         public string RegNumber { get; set; }
         public string TimeParked { get; set; }
         public string DateParked { get; set; }
         public int ParkingSpot { get; set; }
 
-        public Vehicle(string type, string regnumb, int parkspot, DateTime timewhenparked) 
+        public  Vehicle(string type, string regnumb, int parkspot, DateTime timewhenparked) 
         {
             TypeOfVehicle = type;
             RegNumber = regnumb;
