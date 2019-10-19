a84ff0ea7546f04c8487de6f0dcedc5c3b6eebb1
fatha124 <niklas.lund@hotmail.se>
2019-10-19 21:57:27 +0100

    Add files via upload

--- FileController.cs
+++ FileController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
 
 namespace Pragueparking2._01 
 {
     public class FileController
     {
-
+        private static StreamReader ReadFile;
+        private static StreamWriter WriteFile;
     }
 }
 
